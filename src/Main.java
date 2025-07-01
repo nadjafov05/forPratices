19c1382b61cbc5edac78896b48333ac11f9ae962
Perviz Necefov <nadjafovp@gmail.com>
2025-07-01 23:26:46 +0400

    forPratices4

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,34 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public static void tapsiriq4(){
+        for (int i=4;i>=1;i--){
+            for (int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    public static void tapsiriq3(){
+        for (int i=1;i<5;i++){
+            for (int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    public static void tapsiriq1(){
+        for (int i=0;i<5;i++){
+            System.out.print("*");
+        }
+    }
+    public static void tapsiriq2(){
+        for (int j=0;j<4;j++){
+            for (int i=0;i<5;i++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
 }
