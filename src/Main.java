fa8501af42315094ce5295c9248b0cac5f1c3138
Perviz Necefov <nadjafovp@gmail.com>
2025-07-03 00:39:42 +0400

    forPratices5

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,17 @@ public class Main {
 
     }
 
+    public static void tapsiriq5(){
+        for (int i=1;i<=4;i++){
+            for (int j = 1;j<=i-1;j++){
+                System.out.print(" ");
+            }
+            for (int k=1;k<=2*(4-i+1) - 1;k++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
     public static void tapsiriq4(){
         for (int i=4;i>=1;i--){
             for (int j=0;j<i;j++){
