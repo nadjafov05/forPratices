b89db88832e42bdae399250b3d4647392e07fe4a
Perviz Necefov <nadjafovp@gmail.com>
2025-07-03 14:20:36 +0400

    forPratices11

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,41 @@ public class Main {
 
     }
 
+    public static void tapsiriq11(){
+        for (int i=1;i<=10;i++){
+            int netice = i*i*i;
+            System.out.println(i + "->" + netice);
+        }
+    }
+    public static void tapsiriq10(){
+        for (int i=1;i<=100;i++){
+            if (i%3 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+    public static void tapsiriq9(){
+        for (int i=1;i<=10;i++){
+            if (i%2 == 1){
+                System.out.println(i);
+            }
+        }
+    }
+    public static void tapsiriq8(){
+            for (int i=5;i>=1;i--){
+                System.out.println(i);
+            }
+    }
+    public static void tapsiriq7(){
+        for (int i=1;i<=5;i++){
+            System.out.println(i);
+        }
+    }
+    public static void tapsiriq6(){
+        for (int i=1;i<=5;i++){
+            System.out.println("salam dunya");
+        }
+    }
     public static void tapsiriq5(){
         for (int i=1;i<=4;i++){
             for (int j = 1;j<=i-1;j++){
